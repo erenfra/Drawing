2231d1f94d2a04ea91b1620f4478699b32959397
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-01-18 21:50:55 -0600

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Renato Oliveira Fraga on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
