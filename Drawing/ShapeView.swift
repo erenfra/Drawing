fb7696834410e96217d9476e6dac4867bd72c9bb
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-01-19 21:16:29 -0600

    ScoreView Ready

--- Drawing/ShapeView.swift
+++ Drawing/ShapeView.swift
@@ -8,26 +8,7 @@
 import SwiftUI
 
 struct ShapeView: View {
-    
-    struct Arc: Shape {
-        let startAngle: Angle
-        let endAngle: Angle
-        let clockwise: Bool
-        
-        func path(in rect: CGRect) -> Path {
-            var path = Path()
-            path.move(to: CGPoint(x: rect.maxX/3, y: rect.minY))
-            path.addLine(to: CGPoint(x: rect.maxX/3, y: rect.maxY-30))
-            path.addArc(center: CGPoint(x: rect.midX, y: rect.maxY-30), radius: rect.width/6, startAngle: startAngle , endAngle: endAngle, clockwise: clockwise)
-            path.addLine(to: CGPoint(x: 2*(rect.maxX/3), y: rect.minY))
-            path.move(to: CGPoint(x: rect.minX, y: rect.maxY/3.2))
-            path.addLine(to: CGPoint(x: rect.maxX, y: (rect.maxY/3.2)))
-            path.move(to: CGPoint(x: rect.minX, y: (2*(rect.maxY/3)+10)))
-            path.addLine(to: CGPoint(x: rect.maxX, y: (2*(rect.maxY/3)+10)))
-            
-            return path
-        }
-    }
+
     var body: some View {
         
         VStack {
@@ -35,7 +16,7 @@ struct ShapeView: View {
                 .font(.largeTitle.bold())
             Spacer()
             ZStack {
-                Arc(startAngle: .degrees(180), endAngle: .degrees(0), clockwise: true)
+                ScoreGrid(startAngle: .degrees(180), endAngle: .degrees(0), clockwise: true)
                     .stroke(.primary, lineWidth: 10)
                     
                 
